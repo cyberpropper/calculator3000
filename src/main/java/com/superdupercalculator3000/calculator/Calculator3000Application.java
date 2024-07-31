703331e2bfb5dd549bca53499a7416ffb28a37b3
cyberpropper <osankin2002@mail.ru>
2024-07-31 18:40:46 +0700

    11

---
+++ src/main/java/com/superdupercalculator3000/calculator/Calculator3000Application.java
@@ -0,0 +1,12 @@
+package com.superdupercalculator3000.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator3000Application.class, args);
+    }
+}
